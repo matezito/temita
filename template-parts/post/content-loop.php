d7ce064bcc865edb3925276c8e073f791d50f65f
Juan Iriart <jeiriart@gmail.com>
2022-01-27 07:55:00 -0300

    agregado paginacion a los posts

--- template-parts/post/content-loop.php
+++ template-parts/post/content-loop.php
@@ -1,18 +1,21 @@
 <?php
- 
+$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
 $a = [
     'post_type' => $args && $args['post_type'] ? $args['post_type'] : 'post',
+    'post_per_page' => $args && $args['post_per_page'] ? $args['post_per_page'] : get_option('posts_per_page'),
+    'paged' => $paged
 ];
 
-$query = new WP_Query( $a );
+$query = new WP_Query($a);
 
-if($query->have_posts()): 
-    while($query->have_posts()) : $query->the_post();
+if ($query->have_posts()) :
+    while ($query->have_posts()) : $query->the_post();
         the_title('<h2>', '</h2>');
         the_content();
     endwhile;
-else:
-    e( 'Sorry, no posts matched your criteria.', 'temita' );
+    the_posts_pagination();
+else :
+    e('Sorry, no posts matched your criteria.', 'temita');
 endif;
 
 wp_reset_postdata();
